fbecd4238baca63015593ab6f26ee017f5298777
Sudharsan-0-8 <dssudharsan07@gmail.com>
2022-10-22 17:49:52 +0530

    server

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const http = require('http');
 const io = require('socket.io-client');
+
+const server = http.createServer();
 
 const socket = io('http://localhost:3010');
 
+
 socket.on('connect' , () => console.log(socket) ); 
+
+socket.on('error' , () => console.log('error') );
+
+server.listen(4010 , () => console.log('listening to port 4010') );
